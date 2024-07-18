6d5244afdc9ef7fb66cf87e35626b3e80577a47e
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-07-18 20:24:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "spacelift_stack_dependency" "this" {
 
 resource "spacelift_stack_dependency_reference" "this" {
   stack_dependency_id = spacelift_stack_dependency.this.id
-  output_name         = "second_stack_output"
-  input_name          = "TF_VAR_second_stack_output"
+  output_name         = "first_stack_output"
+  input_name          = "TF_VAR_first_stack_output"
 }
 
 resource "spacelift_stack_dependency" "that" {
@@ -16,28 +16,31 @@ resource "spacelift_stack_dependency" "that" {
 
 resource "spacelift_stack_dependency_reference" "that" {
   stack_dependency_id = spacelift_stack_dependency.that.id
-  output_name         = "first_stack_output"
-  input_name          = "TF_VAR_first_stack_output"
+  output_name         = "second_stack_output"
+  input_name          = "TF_VAR_second_stack_output"
 }
 
 
 resource "spacelift_stack" "first_stack" {
   branch     = "main"
   name       = "first stack"
-  repository = "first_stack"
+  repository = "spacelift_outputs_test"
+  project_root      = "first_stack"
 }
 
 
 resource "spacelift_stack" "second_stack" {
   branch     = "main"
   name       = "second stack"
-  repository = "second_stack"
+  repository = "spacelift_outputs_test"
+  project_root      = "second_stack"
 }
 
 resource "spacelift_stack" "third_stack" {
   branch     = "main"
   name       = "third stack"
-  repository = "third_stack"
+  repository = "spacelift_outputs_test"
+  project_root      = "third_stack"
 }
 
 terraform {
@@ -46,14 +49,7 @@ terraform {
       source  = "spacelift.io/spacelift-io/spacelift"
       version = "1.13.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0.0"
-    }
   }
 }
 
 provider "spacelift" {}
-
-provider "aws" {
-}
