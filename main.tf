93e9b37b431b497cd043bd1b81b50818fc2494c9
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-07-18 19:09:18 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "spacelift_stack" "first_stack" {
+  branch     = "main"
+  name       = "first stack"
+  repository = "first_stack"
+}
+
+
+resource "spacelift_stack" "second_stack" {
+  branch     = "main"
+  name       = "second stack"
+  repository = "second_stack"
+}
+
+resource "spacelift_stack_dependency" "test" {
+  stack_id            = spacelift_stack.first_stack.id
+  depends_on_stack_id = spacelift_stack.second_stack.id
+}
+
+resource "spacelift_stack_dependency_reference" "test" {
+  stack_dependency_id = spacelift_stack_dependency.test.id
+  output_name         = "second_stack_output"
+  input_name          = "TF_VAR_second_stack_output"
+}
+
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift.io/spacelift-io/spacelift"
+      version = "1.13.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+provider "aws" {
+}
